b33e6ab588eceb9d3500b7c1de7685b9c8f45be5
unknown <christopherbonime@gmail.com>
2022-12-01 15:22:33 -0500

    added questions for questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,53 @@
+var questions = [
+    {
+        title: "What was the original team name before becoming The Yankees in 1913",
+        choices:["Senators", "Pirates", "Americans", "Highlanders"],
+        answer: "Highlanders"
+    },
+    {
+        title: "Who did the Yankees defeat to win their first world series?",
+        choices: ["Boston", "Giants", "Tigers", "Senators"],
+        answer: "Giants"
+    },
+    {
+        title: "How many world series championships do The Yankees have currently?",
+        choices: ["10", "25", "7", "27"],
+        answer: "27"
+    },
+    {
+        title: "Who is The Yankees single season homerun leader?",
+        choices: ["Ruth", "Judge", "Maris", "Gehrig"],
+        answer: "Judge"
+    },
+    {
+        title: "Who holds The Yankees single season saves record?",
+        choices: ["Chapman", "Lyle", "Rivera", "Gossage"],
+        answer: "Rivera"
+    },
+    {
+        title: "Who holds The Yankees single season strikeout record?",
+        choices: ["Cole", "Guidry", "Cone", "Sabathia"],
+        answer: "Cole"
+    },
+    {
+        title: "Who holds The Yankees team homerun record?",
+        choices: ["Judge", "Maris", "Ruth", "Mantle"],
+        answer: "Ruth"
+    },
+    {
+        title: "What division do The Yankees play?",
+        choices: ["NL East", "AL West", "AL Central", "AL East"],
+        answer: "AL East"
+    },
+    {
+        title: "When was the last season The Yankees posted a losing record?",
+        choices: ["1992", "2008", "2010", "1986"],
+        answer: "1992"
+    },
+    {
+        title: "When did The Yankees win their first World Series?",
+        choices: ["1914", "1920", "1932", "1923"],
+        answer: "1923"
+    }
+    
+]
