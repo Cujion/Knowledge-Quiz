f0ae9514ca12edf10b77749cce60b59a4e772567
unknown <christopherbonime@gmail.com>
2022-12-03 17:37:35 -0500

    added function for displaying first question after start is clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,59 +11,63 @@ var button1 = document.getElementById("button1");
 var button2 = document.getElementById("button2");
 var button3 = document.getElementById("button3");
 var button4 = document.getElementById("button4");
+var notify = document.getElementsByClassName("notify")
 var triviaSubmit = document.getElementById("trivia-submit");
 var submitbutton = document.getElementById("initials-submit");
 var retakeTrivia = document.getElementById("retake-trivia");
 var userInput = document.getElementById("userinput");
 var home = document.getElementById("return");
+var timerStart = 120;
+var usersscores = [];
+var usersinitials = [];
 var questions = [
     {
-        title: "What was the original team name before becoming The Yankees in 1913",
+        question: "What was the original team name before becoming The Yankees in 1913?",
         choices:["Senators", "Pirates", "Americans", "Highlanders"],
         answer: "Highlanders",
     },
     {
-        title: "Who did the Yankees defeat to win their first world series?",
+        question: "Who did the Yankees defeat to win their first world series?",
         choices: ["Boston", "Giants", "Tigers", "Senators"],
         answer: "Giants",
     },
     {
-        title: "How many world series championships do The Yankees have currently?",
+        question: "How many world series championships do The Yankees have currently?",
         choices: ["10", "25", "7", "27"],
         answer: "27",
     },
     {
-        title: "Who is The Yankees single season homerun leader?",
+        question: "Who is The Yankees single season homerun leader?",
         choices: ["Ruth", "Judge", "Maris", "Gehrig"],
         answer: "Judge",
     },
     {
-        title: "Who holds The Yankees single season saves record?",
+        question: "Who holds The Yankees single season saves record?",
         choices: ["Chapman", "Lyle", "Rivera", "Gossage"],
         answer: "Rivera",
     },
     {
-        title: "Who holds The Yankees single season strikeout record?",
+        question: "Who holds The Yankees single season strikeout record?",
         choices: ["Cole", "Guidry", "Cone", "Sabathia"],
         answer: "Cole",
     },
     {
-        title: "Who holds The Yankees team homerun record?",
+        question: "Who holds The Yankees team homerun record?",
         choices: ["Judge", "Maris", "Ruth", "Mantle"],
         answer: "Ruth",
     },
     {
-        title: "What division do The Yankees play?",
+        question: "What division do The Yankees play?",
         choices: ["NL East", "AL West", "AL Central", "AL East"],
         answer: "AL East",
     },
     {
-        title: "When was the last season The Yankees posted a losing record?",
+        question: "When was the last season The Yankees posted a losing record?",
         choices: ["1992", "2008", "2010", "1986"],
         answer: "1992",
     },
     {
-        title: "When did The Yankees win their first World Series?",
+        question: "When did The Yankees win their first World Series?",
         choices: ["1914", "1920", "1932", "1923"],
         answer: "1923",
     }
@@ -77,29 +81,57 @@ startButton.addEventListener('click', function(event) {
         questionContainer.classList.remove("hidden");
         choices.classList.remove("hidden");
     }
+    displayQuestion();
 });
-console.log(questions)
-function showQuestion() {
-    // console.log(questions[9].answer)
-    for (var i = 0; i < questions[0].choices.length; i++) {
-        var button = document.createElement("button")
-        button.innerHTML = questions[i].choices
-        console.log(button)
-        
+
+var qIndex = 0;
+
+function displayQuestion() {
+    if (qIndex === choices.length) {
+        triviaSubmit.classList.remove("hidden");
+        questionContainer.classList.add("hidden");
+
+    } else {
+        triviaQuestions.innerText = questions[qIndex].question;
+        button1.innerHTML = questions[qIndex].choices[0];
+        button2.innerHTML = questions[qIndex].choices[1];
+        button3.innerHTML = questions[qIndex].choices[2];
+        button4.innerHTML = questions[qIndex].choices[3];
     }
 }
 
-showQuestion();
-document.addEventListener("click", function(event) {
-    var element = event.target;
-    if (element.matches("#choices button")) {
+function choiceSelect() {
+    if (element.matches("#choices button") === questions[qIndex].answer) {
+        notify.classList.remove("hidden" + "CORRECT!");
+    }  else {
+        notify.classList.remove("hidden" + "INCORRECT!");
+    } 
+    choiceSelect()
+    displayQuestion();
+}
 
-        // MOVE CURSOR
-        // CHECK FOR CORRECT ANSWER
-        // IF WRONG, SUBTRACT TIME
-        // IF NO MORE QUESTIONS, SHOW END SCREEN
-    }
-});
+// console.log(questions)
+// function showQuestion() {
+//     // console.log(questions[9].answer)
+//     for (var i = 0; i < questions[0].choices.length; i++) {
+//         var button = document.createElement("button")
+//         button.innerHTML = questions[i].choices
+//         console.log(button)
+        
+//     }
+// }
+
+// showQuestion();
+// document.addEventListener("click", function(event) {
+//     var element = event.target;
+//     if (element.matches("#choices button")) {
+
+//         // MOVE CURSOR
+//         // CHECK FOR CORRECT ANSWER
+//         // IF WRONG, SUBTRACT TIME
+//         // IF NO MORE QUESTIONS, SHOW END SCREEN
+//     }
+// });
 
 submitbutton.addEventListener('click', function(event) {
     var element = event.target;
